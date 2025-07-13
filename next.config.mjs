e388355d934b696cfdc0e9c230ab84dc5ee84031
Fredrik Haglund <92490571+fr3h4g@users.noreply.github.com>
2025-07-13 23:10:31 +0200

    update docs

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,6 @@
 const nextConfig = {
   output: "export", // <=== enables static exports
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/docs',
-        destination: '/docs/introduction',
-        permanent: true,
-      },
-    ]
-  },
 };
 
 export default nextConfig;
